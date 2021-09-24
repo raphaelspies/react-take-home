280e54f29fcf31fada2b4e43ae940bdd31773117
raphspies <raphspies@gmail.com>
2021-09-24 13:22:04 -0700

    first commit: retrieves data from endpoint and sets state

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+  const [data, setData] = useState([]);
+  const [isLoaded, setIsloaded] = useState(false)
+
+  const getData = function() {
+    setIsloaded(false)
+    Axios.get('https://www.plugco.in/public/take_home_sample_feed')
+      .then((res) => setData(res))
+      .then(setIsloaded(true))
+      .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="horizontal-app-header">
+        Campaign_icon_url
+        </div>
+        Campaign_name
+        pay_per_install
       </header>
+      <div className="body">
+        Media Object (medias = [...args])
+      </div>
     </div>
   );
 }
